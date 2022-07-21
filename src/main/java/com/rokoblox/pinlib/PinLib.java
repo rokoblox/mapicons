ec58b08c6411831aeb3f3abf96ef7c4cfd96514c
roko <40763333+rokoblox@users.noreply.github.com>
2022-07-21 03:28:40 +0200

    renamed to pinlib and added mixins package

---
+++ src/main/java/com/rokoblox/pinlib/PinLib.java
@@ -0,0 +1,17 @@
+package com.rokoblox.pinlib;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class PinLib implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("PinLib");
+
+
+    /**
+     * This is only used by fabric, do not run.
+     */
+    @Override
+    public void onInitialize() {
+    }
+}
