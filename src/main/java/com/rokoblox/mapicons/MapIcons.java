cc10d34b293b7f93be295c90d915af4e22dedf32
roko <40763333+rokoblox@users.noreply.github.com>
2022-07-21 02:35:31 +0200

    initial commit

---
+++ src/main/java/com/rokoblox/mapicons/MapIcons.java
@@ -0,0 +1,13 @@
+package com.rokoblox.mapicons;
+
+import net.fabricmc.api.ModInitializer;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class MapIcons implements ModInitializer {
+    public static final Logger LOGGER = LogManager.getLogger("MapIcons");
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ testproject/src/main/java/com/rokoblox/testproject/Testproject.java
@@ -0,0 +1,10 @@
+package com.rokoblox.testproject;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Testproject implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
